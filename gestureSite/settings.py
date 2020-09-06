f2ec757fc25f57f1b1e4d93818c10c70c14a0a19
Luis Cubillos <lhcubillos@uc.cl>
2020-09-06 16:16:34 -0300

    added secret key

--- gestureSite/settings.py
+++ gestureSite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'y!7l+(vux@ny4ak((kk=a1^a3)f@&*hn6zssc5a2f=ov^&)ra='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
