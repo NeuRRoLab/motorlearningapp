b7c902b66181b4f3052dba16ffaad502332bc363
Luis Cubillos <lhcubillos@uc.cl>
2021-04-29 08:50:10 -0400

    fixed none keypresses
    Former-commit-id: 1deb0d88edc7579f9c1890aba73ce4ce63ef18f9

--- gestureApp/views.py
+++ gestureApp/views.py
@@ -474,9 +474,10 @@ def download_raw_data(request):
             for x, y in zip(keypresses, keypresses[1:])
         ]
         for values_dict, diff in zip(queryset_list, diff_keypresses_ms):
-            values_dict["keypress_timestamp"] = values_dict[
-                "keypress_timestamp"
-            ].strftime("%Y-%m-%d %H:%M:%S.%f")
+            if values_dict["keypress_timestamp"] is not None:
+                values_dict["keypress_timestamp"] = values_dict[
+                    "keypress_timestamp"
+                ].strftime("%Y-%m-%d %H:%M:%S.%f")
             values_dict["diff_between_keypresses_ms"] = diff
 
         # Output csv
