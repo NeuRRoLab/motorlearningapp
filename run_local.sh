e1e2e7345596855952e0441ed39d1c61d0f2fcf5
Luis Cubillos <lhcubillos@uc.cl>
2022-05-24 18:14:09 -0400

    added script to run app locally
    Former-commit-id: 0d63c9cd575379761ed7d6a93a513380b732eb64

---
+++ run_local.sh
@@ -0,0 +1,7 @@
+sudo service postgres stop
+# Make sure the gcloud shell tool is enabled and connected to a google account with access to the project
+# This way of running the program kills also the connection to the database when exited
+(trap 'kill 0' INT; \
+    ./cloud_sql_proxy -instances="motorlearning:us-central1:motorlearningapp"=tcp:5432 &\
+    (sleep 2 &&\
+    python manage.py runserver))
